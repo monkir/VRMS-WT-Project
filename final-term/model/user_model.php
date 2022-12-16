0ebdd007c7e6daa67ee262eeffbcbbf749c3c73b
Monkir Chowdhury <monkirchowdhury@gmail.com>
2022-12-17 00:09:04 +0600

    Small Update

--- final-term/model/user_model.php
+++ final-term/model/user_model.php
@@ -18,10 +18,10 @@
         {
             $history_details="userid: {$userid}, password: {$password}, usertype: {$usertype}, status: {$status}, name: {$name}, email: {$email}, contact: {$contact}, adress: {$adress}\n";
             addHistory($userid, "registration", $history_details);
-            //storing to file
-            $myfile = fopen("../history_details.txt", "a") or die("Unable to open file!");
-            fwrite($myfile, $history_details);
-            fclose($myfile);
+            // //storing to file
+            // $myfile = fopen("../history_details.txt", "a") or die("Unable to open file!");
+            // fwrite($myfile, $history_details);
+            // fclose($myfile);
 			return true;
 		}
         else
